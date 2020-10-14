436f80f6e403ed2fd9fd7b089ed3f3773ab973dd
minh <nguyengiaminh2k@gmail.com>
2020-10-14 23:41:44 +0700

    update

---
+++ client/src/ItemService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const URL = "http://localhost:4000/items";
+
+class ItemService {
+
+    getItems(){
+        return axios.get(URL);
+    }
+
+    createItem(item){
+        return axios.post(URL + '/create', item);
+    }
+
+    getItemById(itemId){
+        return axios.get(URL + '/edit/' + itemId);
+    }
+
+    updateItem(item, itemId){
+        return axios.put(URL + '/update/' + itemId, item);
+    }
+
+    deleteItem(itemId){
+        return axios.delete(URL + '/delete/' + itemId);
+    }
+}
+
+export default new ItemService()
